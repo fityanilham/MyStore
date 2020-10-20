00ea5e4c468d31e76bb934143730738d674d3fae
fityan ilham <fityanilham9@gmail.com>
2020-10-20 13:57:34 +0700

    Create, Read, Update, Delete, Auth, andsendmassage

---
+++ resources/views/massage.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh.com</title>
+</head>
+<body>
+  <h1>{{ $massage['title'] }}</h1>
+  <p>{{ $massage['body'] }}</p>
+
+  <p>Thank You</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,45 @@ $url = "App\Http\Controllers";
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 // Route::get('product/{slug}', $url . '\ProductController@showProduct');
 // Route::resource('product', $url . '\ProductController');
 
+Route::get('product/{slug}', $url. '\ProductController@showProduct');
+// All Product
+Route::middleware(['auth:sanctum', 'verified'])->get('product', $url. '\ProductController@index');
+
+// Edit
 Route::get('product/edit/{product:product_slug}', $url. '\ProductController@edit');
 Route::patch('product/update', $url. '\ProductController@update');
 
-Route::get('product/{slug}', $url. '\ProductController@showProduct');
-Route::get('product', $url. '\ProductController@index');
-
+// Create
 Route::get('tambah', $url. '\ProductController@tambah');
 Route::post('product/simpan', $url. '\ProductController@simpan');
 
+// Delete
 Route::delete('product/delete/{product:product_slug}', $url. '\ProductController@delete');
+
+// Export
+Route::get('product/export/xlsx', $url. '\ProductController@exportXl');
+Route::get('product/export/csv', $url. '\ProductController@exportCSV');
+Route::get('product/export/pdf', $url. '\ProductController@exportPDF');
+
+// Import
+Route::get('upload', $url. '\ProductController@upload');
+Route::post('product/upload/data', $url. '\ProductController@uploadData');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('kirimEmail', function () {
+    $massage = [
+        'title' => 'Email From My Store',
+        'body' => 'Welcome To My Application'
+    ];
+
+    \Mail::to('fityanilham9@gmail.com')->send(new \App\Mail\EmailKu($massage));
+});
